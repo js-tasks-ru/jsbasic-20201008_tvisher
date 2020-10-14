bf7b603bd92ee2447d7ea828f8adc6c6e0e8141b
Ivan Tikhonov <tvisher@mail.ru>
2020-10-14 12:23:51 +0600

    Дописал функцию антиспама и функцию усеяени строки

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,8 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-  // ваш код...
+  let shortPhrase = str.substr(0, +maxlength - 1);
+  if (str.length > 19) {
+    return shortPhrase + "…"; 
+  } else {return shortPhrase;}
 }
