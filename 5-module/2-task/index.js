6f97620d99e8fa1e9ba299d786298f5e5f18d42f
Ivan Tikhonov <tvisher@mail.ru>
2020-10-30 02:26:28 +0600

    Добавил решение ДЗ по теме Занятие 5: Обработчики событий

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let text = document.querySelector('#text');
+
+  function check() {
+    if (text.hidden == true) text.hidden = false;
+    else text.hidden = true;
+  }
+  btn.addEventListener('click', check);
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
 function initCarousel() {
-  // ваш код...
+  let leftArrow = document.querySelector('.carousel__arrow_left');
+  let rightArrow = document.querySelector('.carousel__arrow_right');
+  let slider = document.querySelector('.carousel__inner');
+  let slide = document.querySelectorAll('.carousel__slide');
+  let shift = 0;
+  let width = slider.offsetWidth;
+  leftArrow.style.display = 'none';
+  rightArrow.onclick = () => {
+    shift += width;
+    slider.style.transform = `translateX(-${shift + 'px'})`;
+    if (shift > 0) {
+      leftArrow.style.display = '';
+    }
+    if (shift == width * (slide.length - 1)) {
+      rightArrow.style.display = 'none';
+    }
+  }
+
+  leftArrow.onclick = () => {
+    shift -= width;
+    slider.style.transform = `translateX(-${shift + 'px'})`;
+    if (shift == 0) {
+      leftArrow.style.display = 'none';
+    }
+    if (shift < width * (slide.length - 1)) {
+      rightArrow.style.display = '';
+    }
+  }
 }
